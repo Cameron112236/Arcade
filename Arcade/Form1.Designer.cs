9e3996499578afe2a0d440809617f4b7226c8f91
Cameron <Camesmal179@ed.amdsb.ca>
2021-05-13 18:24:03 -0400

    Add project files.

---
+++ Arcade/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Arcade
+{
+    partial class arcadeGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(arcadeGame));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.gameStarts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.Black;
+            this.buttonStart.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonStart.FlatAppearance.BorderSize = 2;
+            this.buttonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.buttonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonStart.Location = new System.Drawing.Point(241, 45);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(239, 111);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Play Game";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // gameStarts
+            // 
+            this.gameStarts.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameStarts.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gameStarts.Location = new System.Drawing.Point(200, 271);
+            this.gameStarts.Name = "gameStarts";
+            this.gameStarts.Size = new System.Drawing.Size(318, 109);
+            this.gameStarts.TabIndex = 1;
+            this.gameStarts.Text = "Play in: 3";
+            this.gameStarts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // arcadeGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gameStarts);
+            this.Controls.Add(this.buttonStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "arcadeGame";
+            this.Text = "Arcade";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label gameStarts;
+    }
+}
+
---
+++ Arcade/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace Arcade
+{
+    public partial class arcadeGame : Form
+    {
+        public arcadeGame()
+        {
+            InitializeComponent();
+            gameStarts.Visible = false;
+        }
+   
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // 
+            buttonStart.Visible = false;
+            gameStarts.Visible = true;
+
+            SoundPlayer pingplayer = new SoundPlayer(Properties.Resources.ping);
+            pingplayer.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            pingplayer.Play();
+            gameStarts.Text = "Starts in: 2";
+            Refresh();
+            Thread.Sleep(1000);
+
+            pingplayer.Play();
+            gameStarts.Text = "Starts in: 1";
+            Refresh();
+            Thread.Sleep(1000);
+
+            this.BackColor = Color.Green;
+            gameStarts.Text = "Go!!";
+            SoundPlayer arcadeStartplayer = new SoundPlayer(Properties.Resources.arcadeStart);
+            arcadeStartplayer.Play();
+        }
+    }
+}
